255902c161d1514a66cde7b6776d070c8e2c8061
CrumrineCoder <crumrinecoding@gmail.com>
2018-07-30 14:58:59 -0400

    Data formatted, unnecessary comments removed now

--- src/App.js
+++ src/App.js
@@ -48,24 +48,6 @@ class Article extends React.Component {
     };
   }
 
-  /*
-  createTable = () => {
-    let table = []
-
-    // Outer loop to create parent
-    for (let i = 0; i < 3; i++) {
-      let children = []
-      //Inner loop to create children
-      for (let j = 0; j < 5; j++) {
-        children.push(<td>{`Column ${j + 1}`}</td>)
-      }
-      //Create the parent and add the children
-      table.push(<tr>{children}</tr>)
-    }
-    return table
-  }
-  */
-
   componentDidMount() {
     fetch('https://en.wikipedia.org/w/api.php?action=opensearch&search=dog&limit=10&namespace=0&origin=*')
     .then(results => {
@@ -73,71 +55,24 @@ class Article extends React.Component {
     }).then(data =>{
       let articles = convertToJSON(data).map((article, i) =>{
         return (
-            <h1 key={i}> {article.title} </h1>
+          <a key={i} target="_blank" href={article.link}>
+            <h3> {article.title} </h3>
+            <p> {article.description} </p>
+          </a>
         )
       })
-      console.log(typeof articles);
-      console.log(articles[0]);
      this.setState({magazine: articles});
-     console.log("magazine", this.state.magazine);
-    //  console.log(convertToJSON(data));
-   /*   let table = [];
-
-      for(let i=0; i<3; i++){
-        let children = [];
-        for(let j=0; j<5; j++){
-          children.push(<td>{`Column ${j + 1}`}</td>)
-        }
-        table.push(<tr>{children}</tr>)
-      }
-      return table; */
-  /*    console.log(data);
-     data.map((article, i) => {
-        console.log(article);
-      })
-      this.setState({ titles: data[1], descriptions: data[2], links: data[3] }) */
-   /*   let articles = data.results.map((article, i) =>{
-        return (
-          <div key={i}>
-            <img key={i} src={article.picture.medium} />
-          </div>
-        )
-      })
-      this.setState({ magazine: articles })
-      console.log("magazine", this.state.magazine); */
     })
-     // .then(({ results }) => ;
   }
   render() {
     return (
       //"<a href=" + "'https://en.wikipedia.org/wiki/" + data[1][i] + "' class='entry' id='" + i + "' target='_blank'>" + "<h3>" + data[1][i] + "</h3>" + "<br>" + data[2][i] + "</a>"
-      <a className="entry" href={this.props.link} target="_blank">
+      <div className="entry">
         {this.state.magazine}
-      </a>
+      </div>
     );
   }
-  /*
-  componentDidMount() {
-    fetch("https://en.wikipedia.org/w/api.php?action=opensearch&search=dog&limit=10&namespace=0&format=json")
-      .then(results => {
-        return results.json();
-      }).then(data => {
-        let articles = data.results.map((article) => {
-          return (
-            <div key={data[0]}>
-              <h3>{data[0][0]}</h3>
-            </div>
-          )
-        })
-        this.setState({ magazine: articles });
-        console.log("articles", this.state.magazine);
-      })
-  } */
 }
-
-
-
-
 
 class RandomArticles extends React.Component {
   render() {
